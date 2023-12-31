bfc8bb18b428e59e84c3169d68ac125ca2b8d56b
Kyo SeiKai <phpbbtw@gmail.com>
2023-12-31 23:31:59 +0800

    修正下載問題並增加錯誤處理

--- get.py
+++ get.py
@@ -49,9 +49,12 @@ def download_new_videos(playlist_url, output_directory, history_path):
     """自動下載新影片並更新歷史記錄"""
     new_videos = get_new_videos(playlist_url, history_path)
     playlist = Playlist(playlist_url)
+    print(f"正在處理播放列表：{playlist.title}")
+    print(f"即將下載的新影片：{new_videos}")
     for video_url in new_videos:
         download_video(video_url, output_directory, playlist.title)
         update_download_history(playlist_url, video_url, history_path)
+
 
 # 播放列表 URL
 playlist_urls = [
@@ -59,7 +62,7 @@ playlist_urls = [
     "https://www.youtube.com/watch?v=Y3roUIXa2Fg&list=PL12UaAf_xzfpv_EIuCk3K-WwFSDdYvJiE",
     "https://www.youtube.com/watch?v=fxOE6_bJYrs&list=PL12UaAf_xzfoLF2J08Cpz5Ag2S4mK1J9A",
     "https://www.youtube.com/watch?v=6Lb6NVoF4Jc&list=PL12UaAf_xzfp_uOtg2M8yHR8P6sTzN0ot",
-    "https://www.youtube.com/watch?v=Atp1KzTs4vQ&list=PL12UaAf_xzfqaNNTfkEsrOmfkYtHVZzYl&pp=iAQB",
+    "https://www.youtube.com/watch?v=Atp1KzTs4vQ&list=PL12UaAf_xzfqaNNTfkEsrOmfkYtHVZzYl",
     "https://www.youtube.com/watch?v=BKtsKzr4S24&list=PL12UaAf_xzfrBTYxrhXL6TVvROBkpfQ7K"
 ]
 
